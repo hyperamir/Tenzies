bda0a13fca8817765960d40591a94023d79556a1
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-07 09:39:35 -0500

    create rollDice and holdDice function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Die from './Die';
 import { nanoid } from 'nanoid'
 
 function App() {
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    console.log('dice changed!')
+  },[dice])
+
+  function generateRandomDie() {
+    let randomNum = Math.ceil(Math.random() * 6)
+    return {
+      value: randomNum,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
   function allNewDice() {
     let newDice = [];
     let counter = 0;
     while (counter < 10) {
-      let randomNum = Math.ceil(Math.random() * 6)
-      newDice.push({
-        value: randomNum,
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateRandomDie())
       counter += 1;
     }
     return newDice
   }
 
-  function handleClick() {
-    setDice(allNewDice())
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die => {
+      return !die.isHeld ? generateRandomDie() : die
+    }))
   }
 
   function holdDice(id) {
-    return console.log(id)
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    }))
   }
 
   const diceElements = dice.map(die =>
@@ -39,10 +54,12 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceElements}
       </div>
-      <button className='roll-btn' onClick={handleClick}>ROLL</button>
+      <button className='roll-btn' onClick={rollDice}>ROLL</button>
     </main>
   );
 }
