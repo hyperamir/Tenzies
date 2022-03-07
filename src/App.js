e63c2845a1618abb63f4d59a6637de5d1bb0fd51
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-07 10:53:08 -0500

    confetti functionality and fix the button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import Die from './Die';
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   const [dice, setDice] = useState(allNewDice())
   const [tenzies, setTenzies] = useState(false)
 
   useEffect(() => {
-    console.log('dice changed!')
-  },[dice])
+    const checkIsHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const checkSameValue = dice.every(die => die.value === firstValue)
+    if (checkIsHeld && checkSameValue) {
+      setTenzies(true);
+      console.log('you won!')
+    }
+  }, [dice])
 
   function generateRandomDie() {
     let randomNum = Math.ceil(Math.random() * 6)
@@ -54,12 +61,13 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceElements}
       </div>
-      <button className='roll-btn' onClick={rollDice}>ROLL</button>
+      <button className='roll-btn' onClick={rollDice}>{tenzies ? 'New Game' : 'ROLL'}</button>
     </main>
   );
 }
