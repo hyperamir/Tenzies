7b43e0a89adc85d6cf64deeb3f8faed3943741b8
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-07 08:31:22 -0500

    receive id of clicked-die

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
+import Die from './Die';
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    let newDice = [];
+    let counter = 0;
+    while (counter < 10) {
+      let randomNum = Math.ceil(Math.random() * 6)
+      newDice.push({
+        value: randomNum,
+        isHeld: false,
+        id: nanoid()
+      })
+      counter += 1;
+    }
+    return newDice
+  }
+
+  function handleClick() {
+    setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    return console.log(id)
+  }
+
+  const diceElements = dice.map(die =>
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button className='roll-btn' onClick={handleClick}>ROLL</button>
+    </main>
   );
 }
 
